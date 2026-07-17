1f4de33b360f2beb4756994a0ee0868a8c44351e
israellsalvador <israelplysalvador@gmail.com>
2026-07-17 19:14:20 +0000

    Segundo commit

--- sistema_grafico.sh
+++ sistema_grafico.sh
@@ -1,35 +1,6 @@
 #!/usr/bin/env bash
-#
-# cotarBitcoin.sh - Verifica a última cotação do Bitcoin
-#
-# Site:       https://4fasters.com.br
-# Autor:      Mateus Müller
-# Manutenção: Mateus Müller
-#
-# ------------------------------------------------------------------------ #
-#  Este programa irá cotar o último valor do Bitcoin com base na API xxxx
-#
-#  Exemplos:
-#      $ ./cotarBitcoin.sh -d 1
-#      Neste exemplo o script será executado no modo debug nível 1.
-# ------------------------------------------------------------------------ #
-# Histórico:
-#
-#   v1.0 03/10/2018, Mateus:
-#       - Início do programa
-#       - Conta com a funcionalidade X
-#   v1.1 10/10/2018, Lucas:
-#       - Alterado parametro XXXXX
-# ------------------------------------------------------------------------ #
-# Testado em:
-#   bash 4.4.19
-# ------------------------------------------------------------------------ #
-# Agradecimentos:
-#
-# 	Joãozinho - Encontrou um bug na parte de login.
-#	  Mariazinha - Enviou uma sugestão de adicionar o -h.
-# ------------------------------------------------------------------------ #
-
+# Autor:      Israel salvador
+# Manutenção: Israel Salvador
 # ------------------------------- VARIÁVEIS ----------------------------------------- #
 ARQUIVO_BANCO_DE_DADOS="banco_de_dados.txt"
 SEP=:
